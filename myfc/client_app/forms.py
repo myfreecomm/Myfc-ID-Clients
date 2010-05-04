11112cbf6a50040365abaea0d8c01d75f48c30c2
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2010-05-04 18:02:09 -0300

    Form de login utilizando o novo backend

---
+++ myfc/client_app/forms.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+from django import forms
+from auth import MyfcidAPIBackend
+
+class IdentityAuthenticationForm(forms.Form):
+    """
+    Base class for authenticating users. Extend this to get a form that accepts
+    email/password logins.
+    """
+    email = forms.EmailField(label=_("Email"), max_length=30, required=True)
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        If request is passed in, the form will validate that cookies are
+        enabled. Note that the request (a HttpRequest object) must have set a
+        cookie with the key TEST_COOKIE_NAME and value TEST_COOKIE_VALUE before
+        running this validation.
+        """
+        self.request = request
+        self.user_cache = None
+        super(IdentityAuthenticationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = MyfcidAPIBackend().authenticate(email=email,
+                                                             password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_("Please enter a correct email and password. Note that both fields are case-sensitive."))
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(_("This account is inactive."))
+
+        # TODO: determine whether this should move to its own method.
+        if self.request:
+            if not self.request.session.test_cookie_worked():
+                raise forms.ValidationError(_("Your Web browser doesn't appear to have cookies enabled. Cookies are required for logging in."))
+
+        return self.cleaned_data
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
