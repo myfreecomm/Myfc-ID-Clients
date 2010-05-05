3d9421331d99fb251dce786772663e7a79e32227
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2010-05-05 12:32:53 -0300

    Removendo example_project

--- example_project/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^static/(.*)', 'django.views.static.serve', 
-        {'document_root': settings.MEDIA_ROOT}
-    ),
-)
