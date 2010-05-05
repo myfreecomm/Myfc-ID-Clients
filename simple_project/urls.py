e059fef8d86720f61372ce548a3b7506bce9c3b7
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2010-05-05 13:48:28 -0300

    Adicionando redirect de / para /accounts/login/

--- simple_project/urls.py
+++ simple_project/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
      (r'^accounts/', include('identity_auth_client.urls')),
+     (r'$', 'django.views.generic.simple.redirect_to', {'url': '/accounts/login/'}),
 )
