0ce8eb350625259aae110395c717c15d19086dbf
Vanderson Mota <vanderson@quitandinha.(none)>
2010-07-26 15:22:23 -0300

    testes para a requisição de access token

--- myfc/sso_consumer/tests/test_sso_client.py
+++ myfc/sso_consumer/tests/test_sso_client.py
@@ -9,10 +9,12 @@ from django.conf import settings
 from sso_consumer.views import SSOClient
 
 
-__all__ = ['SSOClientRequestToken']
+__all__ = ['SSOClientRequestToken', 'SSOClientAccessToken']
 
 OAUTH_REQUEST_TOKEN = 'dummyrequesttoken'
 OAUTH_REQUEST_TOKEN_SECRET = 'dummyrequesttokensecret'
+OAUTH_ACCESS_TOKEN = 'dummyaccesstoken'
+OAUTH_ACCESS_TOKEN_SECRET = 'dummyaccesstokensecret'
 
 def mocked_request_token():
     response = Mock()
@@ -20,6 +22,14 @@ def mocked_request_token():
     content = '&'.join(['oauth_token_secret=%s' % OAUTH_REQUEST_TOKEN_SECRET,
                'oauth_token=%s' % OAUTH_REQUEST_TOKEN,
                'oauth_callback_confirmed=true'])
+
+    return response, content
+
+def mocked_access_token():
+    response = Mock()
+    response.status = 200
+    content = '&'.join(['oauth_token_secret=%s' % OAUTH_ACCESS_TOKEN_SECRET,
+               'oauth_token=%s' % OAUTH_ACCESS_TOKEN])
 
     return response, content
 
@@ -79,3 +89,54 @@ class SSOClientRequestToken(TestCase):
         self.assertRaises(HttpLib2Error, self.sso_client.fetch_request_token, oauth_request)
 
 
+class SSOClientAccessToken(TestCase):
+
+    def setUp(self):
+        self.sso_client = SSOClient()
+
+    def build_access_token_request(self, oauth_token, oauth_verifier):
+
+        secret = OAUTH_REQUEST_TOKEN_SECRET
+        token = oauth.Token(key=oauth_token, secret=secret)
+        token.set_verifier(oauth_verifier)
+
+        consumer = oauth.Consumer(settings.SSO['CONSUMER_TOKEN'],
+                                   settings.SSO['CONSUMER_SECRET'])
+        signature_method_plaintext = oauth.SignatureMethod_PLAINTEXT()
+        oauth_request = oauth.Request.from_consumer_and_token(consumer, token=token,
+                     http_url=self.sso_client.access_token_url, parameters={'scope':'sso-sample'})
+
+        return oauth_request
+
+    @patch_object(Http, 'request', Mock(return_value=mocked_access_token()))
+    def test_fetch_access_token_succeeded(self):
+        oauth_token = OAUTH_REQUEST_TOKEN
+        oauth_verifier = 'dummyoauthverifier'
+
+        oauth_request = self.build_access_token_request(oauth_token, oauth_verifier)
+
+        access_token = self.sso_client.fetch_access_token(oauth_request)
+
+        self.assertEqual(OAUTH_ACCESS_TOKEN, access_token.key)
+
+    @patch_object(Http, 'request', Mock(return_value=(401, 'invalid verifier')))
+    def test_fetch_access_token_fails_on_invalid_verifier(self):
+        oauth_token = OAUTH_REQUEST_TOKEN
+        invalid_oauth_verifier = 'invalidoauthverifier'
+
+        oauth_request = self.build_access_token_request(oauth_token, invalid_oauth_verifier)
+
+        access_token = self.sso_client.fetch_access_token(oauth_request)
+
+        self.assertEqual(access_token, None)
+
+    @patch_object(Http, 'request', Mock(return_value=(401, 'invalid token')))
+    def test_fetch_access_token_fails_on_invalid_token(self):
+        invalid_oauth_token = 'invalidtoken'
+        oauth_verifier = 'dummyoauthverifier'
+
+        oauth_request = self.build_access_token_request(invalid_oauth_token, oauth_verifier)
+
+        access_token = self.sso_client.fetch_access_token(oauth_request)
+
+        self.assertEqual(access_token, None)
