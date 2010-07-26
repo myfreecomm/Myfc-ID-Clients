e06414af261ab74e66f7672bc23080ac7a9a98d0
Vanderson Mota <vanderson@quitandinha.(none)>
2010-07-26 17:03:33 -0300

    Preparando testes de view do sso_consumer

--- myfc/sso_consumer/tests/__init__.py
+++ myfc/sso_consumer/tests/__init__.py
@@ -1,3 +1,4 @@
 #coding: utf-8
 
 from test_sso_client import *
+from test_views import *
---
+++ myfc/sso_consumer/tests/test_views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+__all__ = ['SSOConsumerViewsTestCase']
+
+class SSOConsumerViewsTestCase(TestCase):
+    pass
