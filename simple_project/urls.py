8c936f29a3439f62dce2b3d01a990400960d367b
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2011-03-23 19:02:02 -0300

    Adicionando exemplo de utilização de sso por iframe

--- simple_project/urls.py
+++ simple_project/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django.views.generic.simple',
+    url(r'^index.php$',
+        'direct_to_template', {'template': 'iframe.html'},
+        name='iframe'
+    ),
+)
+
+urlpatterns += patterns('',
      (r'^accounts/', include('identity_client.urls')),
      (r'^client-app/', include('example_app.urls')),
      (r'^sso/', include('identity_client.sso_urls', namespace='sso_consumer')),
