01b0a14b95aebdabb8cabb9f018bb0ffe20c2274
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2011-09-22 13:52:15 -0300

    Corrigindo teste que referenciava url que deixou de existir

--- src/myfc/identity_client/tests/test_p3p_middleware.py
+++ src/myfc/identity_client/tests/test_p3p_middleware.py
@@ -18,12 +18,12 @@ class TestP3PMiddleware(TestCase):
 
     @patch.object(middleware, 'settings', enabled_mock)
     def test_p3p_header_is_added(self):
-        response = self.client.get(reverse('iframe'))
+        response = self.client.get(reverse('auth_login'))
         self.assertTrue('P3P' in response)
         self.assertEquals(response['P3P'], 'CP=ANY P3P POL ICY')
 
 
     @patch.object(middleware, 'settings', disabled_mock)
     def test_p3p_header_is_not_added_is_P3P_COMPACT_not_in_settings(self):
-        response = self.client.get(reverse('iframe'))
+        response = self.client.get(reverse('auth_login'))
         self.assertFalse('P3P' in response)
