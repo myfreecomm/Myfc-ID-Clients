07891f717d85ea02df4320a4c14cc417b065bfbf
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2011-09-23 20:26:11 -0300

    Api adicionando membros a account

--- src/myfc/identity_client/api.py
+++ src/myfc/identity_client/api.py
@@ -3,8 +3,9 @@ import json
 from datetime import datetime as dt
 from collections import namedtuple
 
-from django.conf import settings
+from mongoengine import Document, EmbeddedDocument, queryset
 
+from django.conf import settings
 from djangorestframework import status
 from djangorestframework.views import View
 from djangorestframework.resources import Resource
@@ -28,7 +29,7 @@ class AccountActivationAuthenticator(BaseAuthentication):
     def authenticate(self, request):
         expected_token = settings.ACTIVATION_API['TOKEN']
         expected_secret = settings.ACTIVATION_API['SECRET']
-        token, secret = _get_authorization(request)
+        token, secret = self._get_authorization(request)
 
         return Consumer(
             token=token, 
@@ -37,10 +38,61 @@ class AccountActivationAuthenticator(BaseAuthentication):
             is_authenticated=lambda: bool((token, secret) == (expected_token, expected_secret)),
         )
 
+    def _get_authorization(self, request):
+        """ 
+        Extracts the credentials supplied on the HTTP_AUTHORIZATION header
+        """
+        header = request.META.get('HTTP_AUTHORIZATION')
+        if header and header.startswith('Basic '):
+            try:
+                auth_string = header[6:].decode('base64')
+                user, passwd = auth_string.split(':', 1)
+                return user, passwd
+            except:
+                logging.error('parsing (header = %s) - %s: %s',
+                    header, type(e).__name__, e)
+                
+        else:
+            logging.debug('unparsable authorizarion header: %s', header)
+                
+        return None, None
+
 
 class AccountResource(Resource):
 
     fields = ('uuid', 'name', 'expiration', 'is_active')
+
+    def serialize(self, obj):
+
+        if isinstance(obj, (Document, EmbeddedDocument)):
+            return self.serialize_model(obj)
+
+        elif isinstance(obj, (queryset.QuerySet, )):
+            return self.serialize_iter(obj)
+
+        else:
+            return super(AccountResource, self).serialize(obj)
+
+    def serialize_val(self, key, obj):
+        if isinstance(obj, (list, tuple)):
+            return [self.serialize_val(key, v) for v in obj]
+
+        elif isinstance(obj, (dict, )):
+            return dict(
+                (k, self.serialize_val(k, v))
+                for k, v in obj.iteritems()
+            )
+
+        elif hasattr(obj, 'as_dict'):
+            return self.serialize_val(key, obj.as_dict())
+
+        elif isinstance(obj, (EmbeddedDocument, Document)):
+            return self.serialize_val(key, dict(
+                (k, getattr(obj, k)) for k, v in obj._fields.iteritems()
+            ))
+
+        else:
+            return super(AccountResource, self).serialize_val(key, obj)
 
 
 class AccountActivationView(View):
@@ -68,7 +120,7 @@ class AccountActivationView(View):
                 'service_name': data['service_data']['name'],
                 'plan_slug': data['plan_slug'],
                 'expiration': expiration,
-                'members': data['members_data']
+                'members_data': data.get('members_data', {})
             }
 
         except KeyError, e:
@@ -98,6 +150,11 @@ class AccountActivationView(View):
             account.name = data['name']
             account.plan_slug = data['plan_slug']
             account.expiration = data['expiration']
+
+            for item in data['members_data']:
+                identity, created = Identity.objects.get_or_create(uuid=item['identity'])
+                account.add_member(identity, item['roles'])
+
             account.save()
 
         except ErrorResponse, e:
@@ -107,26 +164,7 @@ class AccountActivationView(View):
 
         except Exception, e:
             message = u"Unknown error during activation: Request data is %s" % (data)
+            logging.error(message, request=request)
             raise ErrorResponse(status.HTTP_500_INTERNAL_SERVER_ERROR, content=message)
 
         return account
-
-
-def _get_authorization(request):
-    """ 
-    Extracts the credentials supplied on the HTTP_AUTHORIZATION header
-    """
-    header = request.META.get('HTTP_AUTHORIZATION')
-    if header and header.startswith('Basic '):
-        try:
-            auth_string = header[6:].decode('base64')
-            user, passwd = auth_string.split(':', 1)
-            return user, passwd
-        except:
-            logging.error('parsing (header = %s) - %s: %s',
-                header, type(e).__name__, e)
-            
-    else:
-        logging.debug('unparsable authorizarion header: %s', header)
-            
-    return None, None
--- src/myfc/identity_client/tests/test_account_activation_api.py
+++ src/myfc/identity_client/tests/test_account_activation_api.py
@@ -14,6 +14,7 @@ from identity_client.utils import get_account_module
 
 __all__ = ['TestActivateAccount', 'TestUpdateAccount']
 
+EXPIRATION_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 RemoteAccount = namedtuple('RemoteAccount', [
     'uuid', 'name', 'service_name', 'service_slug', 'plan_slug', 'expiration', 'add_member_url'
@@ -25,7 +26,7 @@ account = RemoteAccount(
     service_name = 'Identity Client',
     service_slug = 'identity_client',
     plan_slug = 'plus',
-    expiration = (dt.today() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+    expiration = (dt.today() + timedelta(days=1)).strftime(EXPIRATION_FORMAT),
     add_member_url = 'http://192.168.1.48:8000/organizations/api/accounts/b39bad59-94af-4880-995a-04967b454c7a/members',
 )
 
@@ -35,7 +36,12 @@ dict_mock = {
     u'expiration': account.expiration,
     u'service_data': {u'name': account.service_name, u'slug': account.service_slug},
     u'plan_slug': account.plan_slug,
-    u'add_member_url': account.add_member_url
+    u'add_member_url': account.add_member_url,
+    u'members_data': [{
+        u'identity': u'e57448c4-41e6-483b-872a-9f32fd15cd63',
+        u'roles': [],
+        u'membership_details_url': u'http://testserver/organizations/api/accounts/2067c5e6-5b77-434d-a43c-abfb24981f93/members/e57448c4-41e6-483b-872a-9f32fd15cd63/',
+    }],
 }
 
 
@@ -173,10 +179,40 @@ class TestActivateAccount(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
-    def test_creates_account_on_success(self):
-        serviceAccount =  get_account_module()
-        self.assertEqual(serviceAccount.objects.count(), 0)
 
+    def test_response_format(self):
+        response = self.client.put(self.url, 
+            json.dumps(dict_mock), 
+            content_type = 'application/json',
+            HTTP_ACCEPT = 'application/json',
+            HTTP_AUTHORIZATION=self.auth
+        )
+
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(data, {
+            u'expiration': account.expiration,
+            u'is_active': True,
+            u'name': u'Pessoal',
+            u'uuid': u'16fd2706-8baf-433b-82eb-8c7fada847da'
+        })
+
+
+    def test_response_format(self):
+        response = self.client.put(self.url, 
+            json.dumps(dict_mock), 
+            content_type = 'application/json',
+            HTTP_ACCEPT = 'application/xml',
+            HTTP_AUTHORIZATION=self.auth
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 
+            '<?xml version="1.0" encoding="utf-8"?>\n<root><expiration>%s</expiration><is_active>True</is_active><uuid>16fd2706-8baf-433b-82eb-8c7fada847da</uuid><name>Pessoal</name></root>' % account.expiration
+        )
+
+
+    def test_creates_account_on_success(self):
         response = self.client.put(self.url, 
             json.dumps(dict_mock), 
             content_type = 'application/json',
@@ -186,6 +222,7 @@ class TestActivateAccount(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        serviceAccount =  get_account_module()
         self.assertEqual(serviceAccount.objects.count(), 1)
         new_account = serviceAccount.objects.get(uuid=account.uuid)
         
@@ -195,13 +232,11 @@ class TestActivateAccount(TestCase):
             )
 
         self.assertEqual(
-            account.expiration, new_account.expiration.strftime('%Y-%m-%d %H:%M:%S')
+            account.expiration, new_account.expiration.strftime(EXPIRATION_FORMAT)
         )
 
-    def test_creates_account_without_expiration(self):
-        serviceAccount =  get_account_module()
-        self.assertEqual(serviceAccount.objects.count(), 0)
 
+    def test_creates_account_without_expiration(self):
         local_mock = dict_mock.copy()
         local_mock['expiration'] = None
 
@@ -214,6 +249,7 @@ class TestActivateAccount(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        serviceAccount =  get_account_module()
         self.assertEqual(serviceAccount.objects.count(), 1)
         new_account = serviceAccount.objects.get(uuid=account.uuid)
         
@@ -225,5 +261,84 @@ class TestActivateAccount(TestCase):
         self.assertEqual(None, new_account.expiration)
 
 
+    def test_updates_account_members_on_success(self):
+        response = self.client.put(self.url, 
+            json.dumps(dict_mock), 
+            content_type = 'application/json',
+            HTTP_ACCEPT = 'application/json',
+            HTTP_AUTHORIZATION=self.auth
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        serviceAccount =  get_account_module()
+        self.assertEqual(serviceAccount.objects.count(), 1)
+        new_account = serviceAccount.objects.get(uuid=account.uuid)
+        
+        self.assertEqual(new_account.members_count, 1)
+        
+
+
 class TestUpdateAccount(TestActivateAccount):
-    pass
+
+    def setUp(self):
+        super(TestUpdateAccount, self).setUp()
+        self.default_plan = 'previous_plan'
+        self.default_expiration = None
+
+        serviceAccount = get_account_module()
+        previous_account = serviceAccount.objects.create(
+            uuid = account.uuid,
+            name = account.name,
+            plan_slug = self.default_plan,
+            expiration =  self.default_expiration,
+        )
+
+    def test_does_not_create_a_new_account(self):
+        response = self.client.put(self.url, 
+            json.dumps(dict_mock), 
+            content_type = 'application/json',
+            HTTP_ACCEPT = 'application/json',
+            HTTP_AUTHORIZATION=self.auth
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        serviceAccount =  get_account_module()
+        self.assertEqual(serviceAccount.objects.count(), 1)
+
+    def test_activation_works_as_plan_change(self):
+        response = self.client.put(self.url, 
+            json.dumps(dict_mock), 
+            content_type = 'application/json',
+            HTTP_ACCEPT = 'application/json',
+            HTTP_AUTHORIZATION=self.auth
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        serviceAccount =  get_account_module()
+        self.assertEqual(serviceAccount.objects.count(), 1)
+        new_account = serviceAccount.objects.get(uuid=account.uuid)
+        
+        self.assertNotEqual(self.default_plan, new_account.plan_slug)
+        self.assertEqual(account.plan_slug, new_account.plan_slug)
+
+    def test_activation_updates_expiration_date(self):
+        response = self.client.put(self.url, 
+            json.dumps(dict_mock), 
+            content_type = 'application/json',
+            HTTP_ACCEPT = 'application/json',
+            HTTP_AUTHORIZATION=self.auth
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        serviceAccount =  get_account_module()
+        self.assertEqual(serviceAccount.objects.count(), 1)
+        new_account = serviceAccount.objects.get(uuid=account.uuid)
+        
+        self.assertNotEqual(self.default_expiration, new_account.expiration)
+        self.assertEqual(
+            account.expiration, new_account.expiration.strftime(EXPIRATION_FORMAT)
+        )
