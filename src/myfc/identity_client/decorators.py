d0eafeb9111c4df12e7b0eadc6b7a7d6bb4a8f49
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2013-01-22 18:48:03 -0200

    Adicionando decorator requires_plan, originario do ecommerce

--- src/myfc/identity_client/decorators.py
+++ src/myfc/identity_client/decorators.py
@@ -3,7 +3,11 @@ from django import http
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import user_passes_test
 
-__all__ = ['required_method', 'sso_login_required', ]
+from requestlogging import logging
+from identity_client.utils import get_account_module
+
+
+__all__ = ['required_method', 'sso_login_required', 'required_method']
 
 
 class required_method(object):
@@ -42,3 +46,35 @@ def sso_login_required(view):
         return wrapped_view(request, *args, **kwargs)
 
     return decorated
+
+
+def requires_plan(plan_slug):
+
+    def decorator(view):
+
+        @sso_login_required
+        def check_user_plan(request, *args, **kwargs):
+            """
+            Este decorator assume que a autenticação acabou de ser feita, 
+            portanto as contas do usuário estão sincronizadas com o Passaporte Web.
+            """
+            serviceAccount = get_account_module()
+            user_accounts = serviceAccount.for_identity(request.user)
+            required_plan_accounts = user_accounts.filter(plan_slug=plan_slug)
+
+            if required_plan_accounts.count() > 0:
+                return view(request, *args, **kwargs)
+            else:
+                logging.info(
+                    'Request from user %s (uuid=%s) denied (%s). User has no accounts at plan "%s".',
+                    request.user.email,
+                    request.user.uuid,
+                    request.path,
+                    plan_slug,
+                    request=request
+                )
+                return http.HttpResponseForbidden()
+
+        return check_user_plan
+
+    return decorator
