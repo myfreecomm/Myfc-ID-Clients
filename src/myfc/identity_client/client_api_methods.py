0e674a1393dad9094be87a78d38f03948dece8ce
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2013-06-24 22:05:04 -0300

    DRY

--- src/myfc/identity_client/client_api_methods.py
+++ src/myfc/identity_client/client_api_methods.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 import logging
 import json
-import requests
 
+import requests
 from django.conf import settings
 
-from identity_client.utils import prepare_form_errors
+from identity_client.decorators import handle_api_exceptions,handle_api_exceptions_with_form
 
 __all__ = ['APIClient']
 
 # TODO: 
 #   - DRY (tratamento de exceções, etc)
-#   - compatibilizar retorno das operações da api
 #   - create_user_account usando uuid
 #   - fetch_application_accounts
 #   - operacoes que o ecommerce precisa
@@ -39,81 +38,30 @@ class APIClient(object):
         'user-agent': 'myfc_id client',
     })
 
-
     @classmethod
+    @handle_api_exceptions_with_form
     def invoke_registration_api(cls, form):
-
-        status_code = 500
-        content = error = error_dict = None
 
         url = "{0}/{1}".format(cls.api_host, cls.registration_api)
 
-        try:
-            registration_data = json.dumps(form.data)
+        logging.info('invoke_registration_api: Making request to %s', url)
 
-            logging.info('invoke_registration_api: Making request to %s', url)
-            response = cls.pweb.post(url, headers={'content-length': str(len(registration_data))}, data=registration_data)
-            status_code = response.status_code
+        registration_data = json.dumps(form.data)
+        response = cls.pweb.post(
+            url,
+            headers={'content-length': str(len(registration_data))},
+            data=registration_data
+        )
 
-            if status_code in (200, 201):
-                content = response.json()
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
+        if response.status_code not in (200, 201):
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code in (400, 409):
-                error_dict = e.response.json()
-            else:
-                error_dict = {
-                    '__all__': [error_messages.get(e.response.status_code, error_messages['default']), ]
-                }
-
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error_dict = {
-                '__all__': [error_messages.get('ConnectionError', error_messages['default']), ]
-            }
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error_dict = {
-                '__all__': [error_messages.get('Timeout', error_messages['default']), ]
-            }
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error_dict = {
-                '__all__': [error_messages.get('default'), ]
-            }
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error_dict:
-            form._errors = prepare_form_errors(error_dict)
-
-        if error:
-            logging.error(
-                'invoke_registration_api: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return (status_code, content, form)
+        return response.status_code, response.json()
 
 
     @classmethod
+    @handle_api_exceptions
     def fetch_identity_data(cls, uuid=None, email=None):
 
         if not any((uuid, email)):
@@ -123,362 +71,123 @@ class APIClient(object):
         else:
             url = "{0}/{1}/?email={2}".format(cls.api_host, cls.profile_api, email)
 
-        status_code = 500
-        content = error = None
+        logging.info('fetch_identity_data: Making request to %s', url)
 
-        try:
-            logging.info('fetch_identity_data: Making request to %s', url)
-            response = cls.pweb.get(url)
-            status_code = response.status_code
+        response = cls.pweb.get(url)
 
-            if response.status_code == 200:
-                content = response.json()
+        if response.status_code != 200:
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
-
-        except requests.exceptions.HTTPError as e:
-
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error:
-            logging.error(
-                'fetch_identity_data: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return status_code, content, error
+        return response.status_code, response.json()
 
 
     @classmethod
+    @handle_api_exceptions_with_form
     def update_user_api(cls, form, api_path):
 
-        status_code = 500
-        content = error = error_dict = None
-
         if api_path.startswith(cls.api_host):
             url = api_path
         else:
             url = "{0}{1}".format(cls.api_host, api_path)
 
-        try:
-            registration_data = json.dumps(form.data)
+        logging.info('update_user_api: Making request to %s', url)
 
-            logging.info('update_user_api: Making request to %s', url)
-            response = cls.pweb.put(url, headers={'content-length': str(len(registration_data))}, data=registration_data)
-            status_code = response.status_code
+        user_data = json.dumps(form.data)
+        response = cls.pweb.put(
+            url,
+            headers={'content-length': str(len(user_data))},
+            data=user_data
+        )
 
-            if status_code == 200:
-                content = response.json()
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
+        if response.status_code != 200:
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code in (400, 409):
-                error_dict = e.response.json()
-            else:
-                error_dict = {
-                    '__all__': [error_messages.get(e.response.status_code, error_messages['default']), ]
-                }
-
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error_dict = {
-                '__all__': [error_messages.get('ConnectionError', error_messages['default']), ]
-            }
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error_dict = {
-                '__all__': [error_messages.get('Timeout', error_messages['default']), ]
-            }
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error_dict = {
-                '__all__': [error_messages.get('default'), ]
-            }
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error_dict:
-            form._errors = prepare_form_errors(error_dict)
-
-        if error:
-            logging.error(
-                'update_user_api: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return (status_code, content, form)
+        return response.status_code, response.json()
 
 
     @classmethod
+    @handle_api_exceptions
     def fetch_association_data(cls, api_path):
 
-        status_code = 500
-        content = error = None
-
         if api_path.startswith(cls.api_host):
             url = api_path
         else:
             url = "{0}{1}".format(cls.api_host, api_path)
 
-        try:
-            logging.info('fetch_association_data: Making request to %s', url)
-            response = cls.pweb.get(url)
-            status_code = response.status_code
+        logging.info('fetch_association_data: Making request to %s', url)
+        response = cls.pweb.get(url)
 
-            if response.status_code == 200:
-                content = response.json()
+        if response.status_code != 200:
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
-
-        except requests.exceptions.HTTPError as e:
-
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error:
-            logging.error(
-                'fetch_association_data: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return status_code, content, error
+        return response.status_code, response.json()
 
 
     @classmethod
+    @handle_api_exceptions
     def update_association_data(cls, new_data, api_path):
 
-        status_code = 500
-        content = error = None
-
         if api_path.startswith(cls.api_host):
             url = api_path
         else:
             url = "{0}{1}".format(cls.api_host, api_path)
 
-        try:
-            logging.info('update_association_data: Making request to %s', url)
-            association_data = json.dumps(new_data)
-            response = cls.pweb.put(url, headers={'content-length': str(len(association_data))}, data=association_data)
-            status_code = response.status_code
+        logging.info('update_association_data: Making request to %s', url)
 
-            if response.status_code == 200:
-                content = response.json()
+        association_data = json.dumps(new_data)
+        response = cls.pweb.put(
+                url,
+                headers={'content-length': str(len(association_data))},
+                data=association_data
+        )
 
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
+        if response.status_code != 200:
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-        except requests.exceptions.HTTPError as e:
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error:
-            logging.error(
-                'update_association_data: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return status_code, content, error
+        return response.status_code, response.json()
 
 
     @classmethod
+    @handle_api_exceptions
     def fetch_user_accounts(cls, uuid, include_expired_accounts=False):
 
-        status_code = 500
-        content = error = None
-
         url = '{0}/organizations/api/identities/{1}/accounts/'.format(cls.api_host, uuid)
 
-        try:
-            logging.info('fetch_user_accounts: Making request to %s', url)
-            response = cls.pweb.get(url, params={
-                'include_expired_accounts': include_expired_accounts,
-            })
-            status_code = response.status_code
+        logging.info('fetch_user_accounts: Making request to %s', url)
+        response = cls.pweb.get(url, params={
+            'include_expired_accounts': include_expired_accounts,
+        })
 
-            if response.status_code == 200:
-                content = response.json()
+        if response.status_code != 200:
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
+        return response.status_code, response.json()
                 
-        except requests.exceptions.HTTPError as e:
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error:
-            logging.error(
-                'fetch_user_accounts: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return status_code, content, error
-
 
     @classmethod
+    @handle_api_exceptions
     def create_user_account(cls, uuid, name, plan_slug, expiration=None):
-
-        status_code = 500
-        content = error = None
 
         url = '{0}/organizations/api/identities/{1}/accounts/'.format(cls.api_host, uuid)
 
-        try:
-            logging.info('create_user_account: Making request to %s', url)
+        logging.info('create_user_account: Making request to %s', url)
 
-            account_data = {'name': name, 'plan_slug': plan_slug, 'expiration': expiration}
-            account_data = json.dumps(account_data)
+        account_data = {'name': name, 'plan_slug': plan_slug, 'expiration': expiration}
+        account_data = json.dumps(account_data)
 
-            response = cls.pweb.post(url, headers={'content-length': str(len(account_data))}, data=account_data)
-            status_code = response.status_code
+        response = cls.pweb.post(
+            url,
+            headers={'content-length': str(len(account_data))},
+            data=account_data
+        )
 
-            if response.status_code in (200, 201):
-                content = response.json()
+        if response.status_code not in (200, 201):
+            response.raise_for_status()
+            raise requests.exceptions.HTTPError('Unexpected response', response=response)
 
-            else:
-                response.raise_for_status()
-                raise requests.exceptions.HTTPError('Unexpected response', response=response)
-
-        except requests.exceptions.HTTPError as e:
-            error = {
-                'status': e.response.status_code,
-                'message': e.response.text if e.response.text else e.message,
-            }
-
-        except requests.exceptions.ConnectionError as e:
-            error = {
-                'status': None,
-                'message': 'Error connecting to PassaporteWeb',
-            }
-
-        except requests.exceptions.Timeout as e:
-            error = {
-                'status': None,
-                'message': 'Timeout connecting to PassaporteWeb',
-            }
-
-        except (requests.exceptions.RequestException, Exception) as e:
-            error = {
-                'status': None,
-                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
-            }
-
-        if error:
-            logging.error(
-                'create_user_account: Error making request: %s - %s',
-                error['status'], error['message']
-            )
-
-        return status_code, content, error
-
-
-error_messages = {
-    401: u"Esta aplicação não está autorizada a utilizar o PassaporteWeb. Entre em contato com o suporte.",
-    400: u"Erro na transmissão dos dados. Tente novamente.",
-    409: u"Email já cadastrado.",
-    'ConnectionError': u"Ocorreu uma falha na comunicação com o Passaporte Web. Por favor tente novamente.",
-    'Timeout': u"Ocorreu uma falha na comunicação com o Passaporte Web. Por favor tente novamente.",
-    'default': u"Erro no servidor. Entre em contato com o suporte.",
-}
+        return response.status_code, response.json()
--- src/myfc/identity_client/decorators.py
+++ src/myfc/identity_client/decorators.py
@@ -1,4 +1,7 @@
 #-*- coding: utf-8 -*-
+from functools import wraps
+
+import requests
 from django import http
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -7,6 +10,7 @@ from django.template import Context, loader
 
 from requestlogging import logging
 from identity_client.utils import get_account_module
+from identity_client.utils import prepare_form_errors
 
 
 __all__ = ['required_method', 'sso_login_required', 'requires_plan', 'with_403_page']
@@ -101,3 +105,125 @@ def with_403_page(view):
         return response
 
     return handle_403
+
+
+def handle_api_exceptions(method):
+
+    @wraps(method)
+    def handler(cls, *args, **kwargs):
+        status_code = 500
+        content = error = None
+
+        try:
+            status_code, content = method(cls, *args, **kwargs)
+
+        except requests.exceptions.HTTPError as e:
+            status_code = e.response.status_code
+            error = {
+                'status': e.response.status_code,
+                'message': e.response.text if e.response.text else e.message,
+            }
+
+        except requests.exceptions.ConnectionError as e:
+            error = {
+                'status': None,
+                'message': 'Error connecting to PassaporteWeb',
+            }
+
+        except requests.exceptions.Timeout as e:
+            error = {
+                'status': None,
+                'message': 'Timeout connecting to PassaporteWeb',
+            }
+
+        except (requests.exceptions.RequestException, Exception) as e:
+            error = {
+                'status': None,
+                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
+            }
+
+        if error:
+            logging.error(
+                'fetch_identity_data: Error making request: %s - %s',
+                error['status'], error['message']
+            )
+
+        return (status_code, content, error)
+
+    return handler
+
+
+def handle_api_exceptions_with_form(method):
+
+    @wraps(method)
+    def handler(cls, form, *args, **kwargs):
+        status_code = 500
+        content = error = error_dict = None
+
+        try:
+            status_code, content = method(cls, form, *args, **kwargs)
+
+        except requests.exceptions.HTTPError as e:
+            status_code = e.response.status_code
+
+            if e.response.status_code in (400, 409):
+                error_dict = e.response.json()
+            else:
+                error_dict = {
+                    '__all__': [error_messages.get(e.response.status_code, error_messages['default']), ]
+                }
+
+            error = {
+                'status': e.response.status_code,
+                'message': e.response.text if e.response.text else e.message,
+            }
+
+        except requests.exceptions.ConnectionError as e:
+            error_dict = {
+                '__all__': [error_messages.get('ConnectionError', error_messages['default']), ]
+            }
+            error = {
+                'status': None,
+                'message': 'Error connecting to passaporteweb',
+            }
+
+        except requests.exceptions.Timeout as e:
+            error_dict = {
+                '__all__': [error_messages.get('Timeout', error_messages['default']), ]
+            }
+            error = {
+                'status': None,
+                'message': 'Timeout connecting to passaporteweb',
+            }
+
+        except (requests.exceptions.RequestException, Exception) as e:
+            error_dict = {
+                '__all__': [error_messages.get('default'), ]
+            }
+            error = {
+                'status': None,
+                'message': u'Unexpected error: {0} <{1}>'.format(e, type(e)),
+            }
+
+        if error_dict:
+            form._errors = prepare_form_errors(error_dict)
+
+        if error:
+            logging.error(
+                '%s: Error making request: %s - %s',
+                method.__name__, error['status'], error['message']
+            )
+
+        return (status_code, content, form)
+
+    return handler
+
+
+error_messages = {
+    401: u"Esta aplicação não está autorizada a utilizar o PassaporteWeb. Entre em contato com o suporte.",
+    400: u"Erro na transmissão dos dados. Tente novamente.",
+    409: u"Email já cadastrado.",
+    'ConnectionError': u"Ocorreu uma falha na comunicação com o Passaporte Web. Por favor tente novamente.",
+    'Timeout': u"Ocorreu uma falha na comunicação com o Passaporte Web. Por favor tente novamente.",
+    'default': u"Erro no servidor. Entre em contato com o suporte.",
+}
